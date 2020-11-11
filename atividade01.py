b4820f7944b9687511ef0fb9e3d4c5a0e6ee7769
baconzitus <thiagolds50@gmail.com>
2020-11-11 17:49:13 -0300

    feat: atividade

---
+++ atividade01.py
@@ -0,0 +1,24 @@
+def leiaint(numero):
+    if numero=="" or numero==None:
+            return 0
+    verifica=True
+    for x in numero:
+        if x in ("0","1","2","3","4","5","6","7","8","9"):
+            pass
+        else:
+            return False
+
+    if verifica==True:
+        return int(numero)
+    else:
+        print("erro inesperado!!")
+            
+
+
+numero = input("numero:")
+numero_verificado= leiaint(numero)
+if (type(numero_verificado)==int):
+    print(numero_verificado)
+else:
+    print("este numero  é inteiro ou sequer é um numero")
+    print(numero)
---
+++ atividade02.py
@@ -0,0 +1,37 @@
+def leiaInt(numero):
+    try:
+        numero_int=int(numero)
+        print(numero_int)
+        return numero_int
+    except(ValueError,TypeError):
+        print("o numero digitado não é um inteiro")
+    return 
+
+def leiaFloat(numero):
+    try:
+        numero_flaot=float(numero)
+        print(numero_flaot)
+        return numero_float
+    except(ValueError,TypeError):
+        print("o numero digitado não é um real")
+    return
+
+
+while True:
+    numero_int=input("\nnumero inteiro: ")
+    leiaInt(numero_int)
+
+    numero_float=input("\nnumero real: ")
+    leiaFloat(numero_float)
+
+
+def leiaReal():
+    try:
+        numero=float(input("numero real: "))
+    except:
+        numero=0
+    return numero
+
+while True:
+    print(leiaReal())
+int(input(">"))
